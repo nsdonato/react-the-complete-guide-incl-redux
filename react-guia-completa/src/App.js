6d9c12bdee113750f615b332a21d79d93815548b
Noelia Donato <noeliadonato@gmail.com>
2019-01-30 21:30:49 -0300

    seccion 3, cap 42

--- react-guia-completa/src/App.js
+++ react-guia-completa/src/App.js
@@ -11,11 +11,24 @@ class App extends Component {
     ]
   }
 
+  switchNameHanlder = () => {
+    // No hay que mutar de esta manera (cambiar de estado), va a dar un error.
+    // NO HAY QUE HACER  ESTO: this.state.persons[0].name = 'Noelia';
+    this.setState({
+      persons: [
+          { name: 'Noels', age: 32 },
+          { name: 'Gabuns', age: 29 },
+          { name: 'Nanys', age: 33 },
+        ]
+      })
+  }
+
+
   render() {
     return (
         <div className="App">
           <h1>Hola, soy una App React</h1>
-          <button>Cambiar de nombre</button>
+          <button onClick={this.switchNameHanlder}>Cambiar de nombre</button>
           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
           <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
           <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
