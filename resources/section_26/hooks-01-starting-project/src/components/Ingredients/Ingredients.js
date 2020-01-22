964890027686737d641f091903fbb060c9ed4ae7
Noelia Donato <noeliadonato@gmail.com>
2020-01-21 22:08:27 -0300

    actualización info

--- resources/section_26/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ resources/section_26/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,34 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
 function Ingredients() {
+  const [ userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    console.log(ingredient)
+    setUserIngredients(prevIngredients => [
+      ...prevIngredients,
+      {id: Math.random().toString(), ...ingredient}
+    ]);
+  }
+
+  const removeItemHandler = ingredient => {
+    let ingredient = user.find(ingredient.id)
+    setUserIngredients(prevIngredients => [
+      ...prevIngredients,
+      {id: Math.random().toString(), ...ingredient}
+    ]);
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeItemHandler} />
       </section>
     </div>
   );
--- resources/section_26/hooks-01-starting-project/src/components/Ingredients/Search.js
+++ resources/section_26/hooks-01-starting-project/src/components/Ingredients/Search.js
@@ -4,12 +4,15 @@ import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const [ enteredFilter, setEnteredFilter ] = useState('');
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input type="text" value={enteredFilter} onChange={event => {
+            setEnteredFilter(event.target.value}/>
         </div>
       </Card>
     </section>
